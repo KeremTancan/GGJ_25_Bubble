6435b223ce8b721972128326cc4c7a0bfd4baf38
Kerem Tancan <tancankerem149@gmail.com>
2025-01-25 02:01:27 +0300

    Added: OrderMenuButton logic

--- Assets/Scripts/OrderManagerUI.cs
+++ Assets/Scripts/OrderManagerUI.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrderManagerUI : MonoSingleton<OrderManagerUI>
 {
     [SerializeField] GameObject orderUIPrefab;
     [SerializeField] Transform orderGroup;
+    [SerializeField] Transform orderParent;
+    [SerializeField] Button orderParentToggleBtn;
 
     
     protected override void Awake()
     {
         base.Awake();
         OrderManager.Instance.OnOrderGenerated += OrderManager_OnOrderGenerated;
+        orderParentToggleBtn.onClick.AddListener(OrderParentToggle);
     }
 
     void OrderManager_OnOrderGenerated(Order order)
@@ -20,5 +24,11 @@ public class OrderManagerUI : MonoSingleton<OrderManagerUI>
         var orderUIGO = Instantiate(orderUIPrefab, orderGroup);
         orderUIGO.GetComponent<OrderUI>().SetOrder(order);
 
+    }
+
+    
+    void OrderParentToggle()
+    {
+        orderParent.gameObject.SetActive(!orderParent.gameObject.activeSelf);
     }
 }
